dbfb0e01cea06da6e0d97cdd6d74e4dffa7b02a5
sky-builder <breakthroughtgw@gmail.com>
2020-03-30 22:50:27 +0800

    dev

--- index.js
+++ index.js
@@ -3,20 +3,33 @@ const app = express();
 const config = require("config");
 const path = require("path");
 const admin = require('firebase-admin');
-let serviceAccount = require('./ebook-272711-9039089b166f.json');
+
+var serviceAccount = require("./ebook-b6021-firebase-adminsdk-efxto-724656caf8.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ebook-b6021.firebaseio.com"
 });
-
 let db = admin.firestore();
 
-let docRef = db.collection('users').doc('alovelace');
-let setAda = docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
-});
+let booksRef = db.collection('books');
+
+
+// let docRef = db.collection('users').doc('alovelace');
+// let setAda = docRef.set({
+//   first: 'Ada',
+//   last: 'Lovelace',
+//   born: 1815
+// });
+// db.collection('users').get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
 
 const PROT = config.get("port");
 
@@ -24,9 +37,38 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  let books = []
+  booksRef.get()
+  .then((snapshot) => {
+    snapshot.forEach(doc => {
+      books.push({
+        ...doc.data(),
+        id: doc.id
+      });
+    })
+    console.log(books)
+    res.render('index', {books})
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
 });
+app.get('/books/:id', (req, res) => {
+  console.log(req.path, 'paht');
+  let book;
+  let { id } = req.params || {};
+  booksRef.doc(id).get()
+  .then((docRef) => {
+    book = docRef.data();
+    book.id = docRef.id;
+    res.render('detail', {book})
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 
 app.listen(PROT, () => {
   console.log(`listening on port ${PROT}`);
 });
+
