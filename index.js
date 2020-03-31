25324049920ce6944227b34bbec3976ee4c0898d
sky-builder <breakthroughtgw@gmail.com>
2020-03-31 19:42:40 +0800

    dev

--- index.js
+++ index.js
@@ -14,46 +14,60 @@ let db = admin.firestore();
 
 let booksRef = db.collection('books');
 
-
-// let docRef = db.collection('users').doc('alovelace');
-// let setAda = docRef.set({
-//   first: 'Ada',
-//   last: 'Lovelace',
-//   born: 1815
-// });
-// db.collection('users').get()
-//   .then((snapshot) => {
-//     snapshot.forEach((doc) => {
-//       console.log(doc.id, '=>', doc.data());
-//     });
-//   })
-//   .catch((err) => {
-//     console.log('Error getting documents', err);
-//   });
-
 const PROT = config.get("port");
+const PAGE_SIZE = config.get('pageSize')
+const TOTAL_DOC_ID = config.get('totalDocId')
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded())
 
-app.get("/", (req, res) => {
-  let { keyword } = req.query || {};
+app.get("/", async (req, res) => {
+  let { q, p } = req.query || {};
+  let s = PAGE_SIZE;
+  p = parseInt(p);
+  if (isNaN(p)) p = 1;
   let query;
-  if (keyword) {
-    query = booksRef.where('name', '==', keyword).get();
+  if (q) {
+    query = booksRef.where('name', '==', q).get()
   } else {
-    query = booksRef.get();
+    if (p === 1) query = booksRef.orderBy('name').limit(s).get();
+    else {
+      let skipCount = (p - 1) * s;
+      let first = booksRef.orderBy('name').limit(skipCount);
+      let snapshot = await first.get();
+      let last = snapshot.docs[snapshot.docs.length - 1];
+      query = booksRef.orderBy('name').startAfter(last.data().name).limit(s).get();
+    }
   }
+  let totalQuery = booksRef.doc(TOTAL_DOC_ID).get()
+  Promise.all([query, totalQuery])
+  .then(data => {
+    let books = []
+    let [docs, totalDoc] = data;
+    docs.forEach(doc => {
+      books.push({
+        ...doc,data(),
+        id: doc.id,
+      })
+    })
+    let totalCount = totalDoc.data().total;
+    let payload = {
+      books: books,
+      q: q,
+      p: p,
+      s: s,
+      totalPage: 
+    }
+    res.render('index', {books, q: q, totalPage: 2, s: 2, p: p});
+  })
   query
   .then((snapshot) => {
     snapshot.forEach(doc => {
-      let books = []
       books.push({
         ...doc.data(),
         id: doc.id
       });
-      res.render('index', {books})
     })
   })
   .catch((err) => {
@@ -61,7 +75,6 @@ app.get("/", (req, res) => {
   });
 });
 app.get('/books/:id', (req, res) => {
-  console.log(req.path, 'paht');
   let book;
   let { id } = req.params || {};
   booksRef.doc(id).get()
